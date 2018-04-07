66f531078a5a11ef927ea6983fdd7282215431ab
Kevin <kph1958@rit.edu>
2018-04-07 15:43:30 -0400

    Capturing line number and line

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,4 @@
+#[macro_use] extern crate lazy_static;
 extern crate grep;
 extern crate walkdir;
 extern crate memmap;
@@ -10,38 +11,46 @@ use walkdir::WalkDir;
 use grep::Grep;
 use grep::GrepBuilder;
 use grep::Match;
+use std::ops::Deref;
+use std::io::BufRead;
 
 use memmap::Mmap;
 
 fn should_ignore(path: &str) -> bool {
-    let re = Regex::new(r"^(\./|/)target|/\..*").expect("Unable to parse regex");
-    return re.find(path).is_some();
+    // This lets us only instantiate the regex one time, which is expensive
+    lazy_static! {
+        static ref RE: Regex = Regex::new(r"^(\./|/)target|/\..*")
+            .expect("Unable to parse regex");
+    }
+
+    return RE.find(path).is_some();
 }
 
-fn search_file(file: &File, search: &Grep) -> bool {
+fn search_file(file: &File, search: &Grep) -> Result<Vec<(u32, String)>, String> {
     let mmap = unsafe { Mmap::map(&file) };
     if mmap.is_err() {
-        eprintln!("failed to map the file: {}", mmap.unwrap_err());
-        return false;
+        return Err(format!("failed to map the file: {}", mmap.unwrap_err()));
     }
 
+    let mut findings = Vec::new();
+
+    let mut line_number = 0;
     let mmap = mmap.unwrap();
-
-    let mut found_at_least_one = false;
-    let mut the_match = Match::new();
-    let mut start_index = 0;
-    loop {
-        let found = search.read_match(&mut the_match, &mmap[..], start_index);
-        if found {
-            found_at_least_one = true;
-            println!("{:?}", the_match);
-            start_index = the_match.end();
-        } else {
-            break;
+    for line in mmap.deref().lines() {
+        if line.is_err() {
+            continue;
         }
+
+        let line = line.unwrap();
+        let mut the_match = Match::new();
+        let found = search.read_match(&mut the_match, line.as_bytes(), 0);
+        if found {
+            findings.push( (line_number, line) );
+        }
+        line_number += 1;
     }
 
-    return found_at_least_one;
+    return Ok(findings);
 }
 
 fn walk_dir(dir: &str, search: &Grep) {
@@ -69,8 +78,10 @@ fn walk_dir(dir: &str, search: &Grep) {
         }
 
         let file = file.unwrap();
-        if search_file(&file, &search) {
+        let findings = search_file(&file, search).unwrap_or(Vec::new());
+        if !findings.is_empty() {
             println!("{}", entry.path().display());
+            println!("{:?}", findings);
         }
     }
 }
@@ -80,8 +91,8 @@ fn main() {
         .nth(1)
         .unwrap_or(String::from("."));
 
-    let search = GrepBuilder::new("mongoose")
+    let ref_search = GrepBuilder::new("~tr:")
         .build()
-        .expect("Unable to build search");
-    walk_dir(&path, &search);
+        .expect("Unable to build reference search");
+    walk_dir(&path, &ref_search);
 }
