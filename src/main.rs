2b0191535e86ef6ad32c4b23759ae76067e0c979
Kevin Hannigan <kph1958@rit.edu>
2018-04-16 12:04:24 -0400

    Parsing arequirement file

--- src/main.rs
+++ src/main.rs
@@ -92,13 +92,83 @@ fn walk_dir(dir: &str, search: &Grep) {
     }
 }
 
+// Read in requirements yaml files, linting for correctness
+#[derive(Serialize, Deserialize, Debug)]
+struct Requirement {
+    id: String, // The prefix before the trace in source files
+    name: String,
+    details: Option<String>,
+    parent: Option<String>,
+}
+#[derive(Serialize, Deserialize, Debug)]
+struct RequirementCollection {
+    requirements: Vec<Requirement>,
+}
+impl RequirementCollection {
+    fn new() -> RequirementCollection {
+        RequirementCollection { requirements: Vec::new() }
+    }
+}
+
+fn parse_requirements(dir: String) -> RequirementCollection {
+    let mut collection = RequirementCollection::new();
+    for entry in WalkDir::new(dir) {
+        if entry.is_err() {
+            eprintln!("{}", entry.unwrap_err());
+            continue;
+        }
+
+        let entry = entry.unwrap();
+
+        // Don't run on non-files
+        if !entry.file_type().is_file() {
+            continue;
+        }
+
+        // Skip ignore paths
+        let extension = entry.path().extension();
+        if extension.is_none() || extension.unwrap() != "yml" {
+            continue;
+        }
+
+        let file = File::open(entry.path());
+
+        // Skip if we failed to open the file
+        if file.is_err() {
+            eprintln!("Unable to open {}: {}",
+                      entry.path().display(),
+                      file.unwrap_err());
+            continue;
+        }
+
+        let file = file.unwrap();
+        let requirement: Result<Requirement, serde_yaml::Error> =
+            serde_yaml::from_reader(file);
+
+        if requirement.is_err() {
+            eprintln!("Unable to parse {}: {}",
+                      entry.path().display(),
+                      requirement.unwrap_err());
+            continue;
+        }
+
+        let requirement = requirement.unwrap();
+        collection.requirements.push(requirement);
+    }
+    return collection;
+}
+
 #[derive(Serialize, Deserialize, Debug)]
 struct TraceConfig {
     identifier: String, // The prefix before the trace in source files
+    requirements_dir: String,
 }
 impl TraceConfig {
     fn new() -> TraceConfig {
-        TraceConfig { identifier: String::from("~tr:") }
+        TraceConfig {
+            identifier: String::from("~tr:"),
+            requirements_dir: String::from("req"),
+        }
     }
 }
 fn read_config() -> TraceConfig {
@@ -116,12 +186,14 @@ fn read_config() -> TraceConfig {
 
 fn main() {
     let config = read_config();
-
     println!("{:?}", config);
 
     let path = std::env::args()
         .nth(1)
         .unwrap_or(String::from("."));
+
+    let requirements = parse_requirements(format!("{}/{}", path, config.requirements_dir));
+    println!("{:?}", requirements);
 
     let ref_search = GrepBuilder::new(config.identifier.as_str())
         .build()
